baa6ed24a77400cef0e6278eace2d4ce63c9c207
JeanEspindola <jean.espindola@gmail.com>
2017-12-02 16:59:17 -0200

    Url-Shortener | App Adjusting App class.

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import LinkInputForm from './link-input-form/LinkInputForm';
+import Header from './Header/Header';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>A Simple React Component Example</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Header />
+
+    <LinkInputForm />
+
+  </div>
+);
+
+export default App;
+
+/*
+ class App extends React.Component {
+ render() {
+ return (
+ <div>
+ <Header />
+
+ <LinkInputForm />
+
+ </div>
+ );
+ }
+ }*/

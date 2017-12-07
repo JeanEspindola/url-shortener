19d55a69fa2e89a84d1374060ee80efa4e7300fa
JeanEspindola <jean.espindola@gmail.com>
2017-12-07 21:41:18 -0200

    Url-Shortener | React-Bootstrap Adding react-bootstrap dependency. Importing css directly on the head. Adding grid to header component.

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 const Header = () => (
   <header className="header">
-    <div>
-      <h1 className="header__logo">Shooooort</h1>
-      <span className="header__text">The link shortener with a long name</span>
-    </div>
+    <Grid fluid={true}>
+      <Row>
+        <Col md={8}>
+          <h1 className="header__logo">Shooooort</h1>
+        </Col>
+        <Col md={4}>
+          <span className="header__text">The link shortener with a long name</span>
+        </Col>
+      </Row>
+    </Grid>
   </header>
 );
 
